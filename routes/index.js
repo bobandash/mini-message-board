fd435d42e2186a3cdf97df4bae1ccd8912572042
Bruce Hsu <brucehsu1126@gmail.com>
2023-09-28 15:53:08 -0700

    add router/new and messages array

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+]
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/new", function(req, res) {
+  res.send("<p>Hi</p>");
+})
 
 module.exports = router;
