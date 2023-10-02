121750edc8453b3765e79c179f9ce2846f5bebf2
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-02 16:28:04 -0700

    add message model

---
+++ models/message.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const {DateTime} = require('luxon');
+
+const MessageSchema = new Schema({
+  date_of_message: {
+    type: String,
+  },
+  username: {
+    type: String,
+    required: true,
+    maxLength: 100,
+  },
+  message: {
+    type: String,
+    required: true,
+    maxLength: 1000,
+  },
+})
+
+
+MessageSchema.virtual('date_of_message_formatted').get(function(){
+  return this.date_of_message ? DateTime.fromJSDate(this.date_of_message).toLocaleString(DateTime.DATE_MED) : '';
+})
