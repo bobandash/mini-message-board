a140769ab66357cfc5ab1f89e642eea1bd19f6f4
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-04 19:57:14 -0700

    change code to controller functions

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,10 @@
-let express = require('express');
-let router = express.Router();
-
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-]
+const express = require("express");
+const router = express.Router();
+const messageController = require('../controller/messageController');
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
-});
+router.get('/', messageController.messages_get);
 
-router.get("/new", function(req, res) {
-  res.render('form', {title: 'Add Message'})
-})
-
-router.post("/new", function(req, res) {
-  const messageText = req.body["message-text"];
-  const messageUser = req.body["author-name"];
-  messages.push({text: messageText, user: messageUser, added: new Date()});
-  res.redirect("/");
-})
+router.post('/', messageController.messages_post);
 
 module.exports = router;
