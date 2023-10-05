9c385774c1d28888d5030c56afd4bf2ecf096f13
Bruce Hsu <brucehsu1126@gmail.com>
2023-10-04 19:56:11 -0700

    add connection to database

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config();
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const bodyParser = require('body-parser');
 
 var app = express();
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+async function connectDatabase(){
+  try {
+    await mongoose.connect(`mongodb+srv://brucehsu1126:${process.env.MESSAGE_DATABASE_PASSWORD}@cluster0.abjl5jr.mongodb.net/?retryWrites=true&w=majority`);
+  } catch(err) {
+    console.log(err);
+  }
+}
+connectDatabase();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,12 +25,11 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
